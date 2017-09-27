2ec8e2637de9aa2878631e341c4d3c7dbaa578a1
sam <sampenland@gmail.com>
2017-09-27 16:40:03 -0400

    Added abiliy to paste URL which parses into built section
    - Also, UDP reception triggers cloud posting

--- app/src/main/java/www/callerid/com/androidcalleridcloudrelay/actMain.java
+++ app/src/main/java/www/callerid/com/androidcalleridcloudrelay/actMain.java
@@ -1,6 +1,8 @@
 package www.callerid.com.androidcalleridcloudrelay;
 
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -124,14 +126,14 @@ public class actMain extends Activity implements ServiceCallbacks {
 
         // Attach event handlers
         ckbRequiresAuth.setOnClickListener(ckbRequiresAuth_Click);
-
         rbUseBuiltURL.setOnClickListener(rbChangeToBuilt);
         rbUseSuppliedURL.setOnClickListener(rbChangeToSupplied);
-
         rbDeluxeUnit.setOnClickListener(rbChangeToDeluxe);
         rbBasicUnit.setOnClickListener(rbChangeToBasic);
+        btnGenerateURL.setOnClickListener(btnGenerateURL_Click);
+        btnPaste.setOnClickListener(btnPaste_Click);
 
-        // Update UI based on loaded in values
+        // Update UI based on restored settings values
         ckbRequiresAuth_Click.onClick(new View(this));
         changeURL(rbUseSuppliedURL.isChecked());
         changeUnit(rbDeluxeUnit.isChecked());
@@ -199,17 +201,21 @@ public class actMain extends Activity implements ServiceCallbacks {
         saveSettings();
 
     }
-    // ------------------------------------------------------------------------------------------------------------- Settings
+    // ---------------------------------------------------------------------------------------------------- Setting Variables
 
     // Setting vars
     public static final String PREFS_NAME = "Cloud_Relay_Saved_Prefs";
+
     private String savedRequireAuth = "spRequiresAuth";
     private String savedUserName = "spUsername";
     private String savedPassword = "spPassword";
+
     private String savedIsSupplied = "spIsSupplied";
     private String savedIsDeluxe = "spIsDeluxe";
+
     private String savedBuiltURL = "spBuiltURL";
     private String savedServer = "spServer";
+
     private String savedParam_Line = "spParamLine";
     private String savedParam_Time = "spParamTime";
     private String savedParam_Phone = "spParamPhone";
@@ -220,6 +226,8 @@ public class actMain extends Activity implements ServiceCallbacks {
     private String savedParam_Duration = "spParamDuration";
     private String savedParam_RingNumber = "spParamRingNumber";
     private String savedParam_RingType = "spParamRingType";
+
+    // ---------------------------------------------------------------------------------------------------- Setting Functions
 
     private void saveSettings(){
 
@@ -392,6 +400,18 @@ public class actMain extends Activity implements ServiceCallbacks {
     private View.OnClickListener rbChangeToBasic = new View.OnClickListener() {
         public void onClick(View v) {
             changeUnit(false);
+        }
+    };
+
+    private View.OnClickListener btnGenerateURL_Click = new View.OnClickListener() {
+        public void onClick(View v) {
+            generateURL();
+        }
+    };
+
+    private View.OnClickListener btnPaste_Click = new View.OnClickListener() {
+        public void onClick(View v) {
+            pasteUrl();
         }
     };
 
@@ -707,7 +727,8 @@ public class actMain extends Activity implements ServiceCallbacks {
     }
 
     // ------------------------------------------------------------------------------------------- Actual POST to Cloud code
-    private void PostToCloud(String urlFull, String line, String dateTime, String number, String name, String io,
+
+    private void postToCloud(String urlFull, String line, String dateTime, String number, String name, String io,
                              String se, String status, String duration, String ring){
 
         Boolean secured = urlFull.contains("https://");
@@ -722,9 +743,244 @@ public class actMain extends Activity implements ServiceCallbacks {
 
     }
 
+    // ------------------------------------------------------------------------------- Regex Patterns for Parsing Pasted URL
+
+    // Patterns for parsing pasted URL
+    Pattern linePattern = Pattern.compile("([&]?([A-Za-z0-9_-]+)=%Line)");
+    Pattern ioPattern = Pattern.compile("([&]?([A-Za-z0-9_-]+)=%IO)");
+    Pattern sePattern = Pattern.compile("([&]?([A-Za-z0-9_-]+)=%SE)");
+    Pattern durationPattern = Pattern.compile("([&]?([A-Za-z0-9_-]+)=%Duration)");
+    Pattern ringTypePattern = Pattern.compile("([&]?([A-Za-z0-9_-]+)=%RingType)");
+    Pattern ringNumberPattern = Pattern.compile("([&]?([A-Za-z0-9_-]+)=%RingNumber)");
+    Pattern timePattern = Pattern.compile("([&]?([A-Za-z0-9_-]+)=%Time)");
+    Pattern phonePattern = Pattern.compile("([&]?([A-Za-z0-9_-]+)=%Phone)");
+    Pattern namePattern = Pattern.compile("([&]?([A-Za-z0-9_-]+)=%Name)");
+    Pattern statusPattern = Pattern.compile("([&]?([A-Za-z0-9_-]+)=%Status)");
+
+    // ---------------------------------------------------------------------------------- Parsing and Building URL functions
+
+    private void pasteUrl(){
+
+        ClipboardManager clipboard = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+        ClipData clipData = clipboard.getPrimaryClip();
+        ClipData.Item item = clipData.getItemAt(0);
+        String fullURL = item.getText().toString();
+
+        if (!fullURL.contains("?"))
+        {
+            // -- TODO show message "Incorrect format. The text on clipboard does not contain a '?' which is required."
+            return;
+        }
+
+        String[] urlParts = fullURL.split("\\?");
+
+        String allParams = urlParts[1];
+
+        if (allParams.isEmpty())
+        {
+            // -- TODO show message "No Parameters Found. The text on clipboard does not contain text after '?'."
+            return;
+        }
+
+        int parameters = 0;
+
+        Matcher m = linePattern.matcher(allParams);
+        if (m.matches()) {
+            tbLine.setText(m.group(2));
+            parameters++;
+        }
+
+        m = timePattern.matcher(allParams);
+        if (m.matches()) {
+            tbTime.setText(m.group(2));
+            parameters++;
+        }
+
+        m = phonePattern.matcher(allParams);
+        if (m.matches()) {
+            tbPhone.setText(m.group(2));
+            parameters++;
+        }
+
+        m = namePattern.matcher(allParams);
+        if (m.matches()) {
+            tbName.setText(m.group(2));
+            parameters++;
+        }
+
+        m = ioPattern.matcher(allParams);
+        if (m.matches()) {
+            tbIO.setText(m.group(2));
+            parameters++;
+        }
+
+        m = sePattern.matcher(allParams);
+        if (m.matches()) {
+            tbSE.setText(m.group(2));
+            parameters++;
+        }
+
+        m = statusPattern.matcher(allParams);
+        if (m.matches()) {
+            tbStatus.setText(m.group(2));
+            parameters++;
+        }
+
+        m = durationPattern.matcher(allParams);
+        if (m.matches()) {
+            tbDuration.setText(m.group(2));
+            parameters++;
+        }
+
+        m = ringNumberPattern.matcher(allParams);
+        if (m.matches()) {
+            tbRingNumber.setText(m.group(2));
+            parameters++;
+        }
+
+        m = ringTypePattern.matcher(allParams);
+        if (m.matches()) {
+            tbRingType.setText(m.group(2));
+            parameters++;
+        }
+
+        if (parameters < 1)
+        {
+            // -- TODO show message "No Parameters Parsed. There were no parameters that could be parsed."
+
+        }
+        else
+        {
+            // -- TODO show message "Paste Complete. Clipboard text succesfully parsed into your Developer Section."
+
+            tbSuppliedURL.setText(fullURL);
+        }
+    }
+
+    private void generateURL(){
+
+        StringBuilder genUrl = new StringBuilder();
+
+        if(tbServer.getText().toString()==""){
+
+            String failed = "[ previous generation failed - fill out server ]";
+            lbGeneratedURL.setText(failed);
+
+            tbServer.setHighlightColor(Color.RED);
+            tbServer.setTextColor(Color.BLACK);
+
+            // -- TODO show failed message ""Server Cannot be blank. Please input your Cloud Server."
+
+            return;
+
+        }
+
+        tbServer.setHighlightColor(Color.WHITE);
+        tbServer.setTextColor(Color.GREEN);
+
+        String urlStart = tbServer.getText().toString() + "?";
+        genUrl.append(urlStart);
+
+        int parameters = 0;
+
+        // Line
+        if (!tbLine.getText().toString().isEmpty())
+        {
+            parameters++;
+            String combo = tbLine.getText().toString() + "=%Line&";
+            genUrl.append(combo);
+        }
+
+        // Time
+        if (!tbTime.getText().toString().isEmpty())
+        {
+            parameters++;
+            String combo = tbTime.getText().toString() + "=%Time&";
+            genUrl.append(combo);
+        }
+
+        // Phone
+        if (!tbPhone.getText().toString().isEmpty())
+        {
+            parameters++;
+            String combo = tbPhone.getText().toString() + "=%Phone&";
+            genUrl.append(combo);
+        }
+
+        // Name
+        if (!tbName.getText().toString().isEmpty())
+        {
+            parameters++;
+            String combo = tbName.getText().toString() + "=%Name&";
+            genUrl.append(combo);
+        }
+
+        // IO
+        if (!tbIO.getText().toString().isEmpty())
+        {
+            parameters++;
+            String combo = tbIO.getText().toString() + "=%IO&";
+            genUrl.append(combo);
+        }
+
+        // SE
+        if (!tbSE.getText().toString().isEmpty())
+        {
+            parameters++;
+            String combo = tbSE.getText().toString() + "=%SE&";
+            genUrl.append(combo);
+        }
+
+        // Status
+        if (!tbStatus.getText().toString().isEmpty())
+        {
+            parameters++;
+            String combo = tbStatus.getText().toString() + "=%Status&";
+            genUrl.append(combo);
+        }
+
+        // Duration
+        if (!tbDuration.getText().toString().isEmpty())
+        {
+            parameters++;
+            String combo = tbDuration.getText().toString() + "=%Duration&";
+            genUrl.append(combo);
+        }
+
+        // RingNumber
+        if (!tbRingType.getText().toString().isEmpty())
+        {
+            parameters++;
+            String combo = tbRingNumber.getText().toString() + "=%RingNumber&";
+            genUrl.append(combo);
+        }
+
+        // RingType
+        if (!tbRingType.getText().toString().isEmpty())
+        {
+            parameters++;
+            String combo = tbRingType.getText().toString() + "=%RingType&";
+            genUrl.append(combo);
+        }
+
+        if (parameters == 0)
+        {
+            lbGeneratedURL.setTextColor(Color.RED);
+            String failed = "[previous generation failed - use at least one parameter]";
+            lbGeneratedURL.setText(failed);
+
+            // -- TODO show message ""No Parameters Set. You must use at least one parameter."
+
+            return;
+        }
+
+        lbGeneratedURL.setTextColor(Color.GREEN);
+        lbGeneratedURL.setText(genUrl.toString().substring(0,genUrl.toString().length()-1));
+
+    }
+
     // ------------------------------------------------------------------------------------------------ UDP LOWER Level Code
 
-    // Link Display to Update so the UI gets updated through interface
     @Override
     public void getUDP(String rString){
 
@@ -746,15 +1002,15 @@ public class actMain extends Activity implements ServiceCallbacks {
         // Setup variables for use
         String myData = received;
 
-        Integer myLine;
-        String myType;
-        String myIndicator;
-        String myDuration;
-        String myCheckSum;
-        String myRings;
+        String myLine = "";
+        String myType = "";
+        String myIndicator = "";
+        String myDuration = "";
+        String myCheckSum = "";
+        String myRings = "";
         String myDateTime = "";
-        String myNumber;
-        String myName;
+        String myNumber = "";
+        String myName = "";
 
         // Check if matches a call record
         Pattern myPattern = Pattern.compile(".*(\\d\\d) ([IO]) ([ES]) (\\d{4}) ([GB]) (.)(\\d) (\\d\\d/\\d\\d \\d\\d:\\d\\d [AP]M) (.{8,15})(.*)");
@@ -762,7 +1018,7 @@ public class actMain extends Activity implements ServiceCallbacks {
 
         if(matcher.find()){
 
-            myLine = Integer.parseInt(matcher.group(1));
+            myLine = matcher.group(1);
             myType = matcher.group(2);
 
             if(myType.equals("I")||myType.equals("O")){
@@ -779,7 +1035,7 @@ public class actMain extends Activity implements ServiceCallbacks {
                 myNumber = matcher.group(9);
                 myName = matcher.group(10);
 
-                String theLineNumber = myLine.toString();
+                String theLineNumber = myLine;
                 if(theLineNumber.length() == 1) theLineNumber = "0" + theLineNumber;
 
                 // Add to log
@@ -793,21 +1049,54 @@ public class actMain extends Activity implements ServiceCallbacks {
         Pattern myPatternDetailed = Pattern.compile(".*(\\d\\d) ([NFR]) {13}(\\d\\d/\\d\\d \\d\\d:\\d\\d:\\d\\d)");
         Matcher matcherDetailed = myPatternDetailed.matcher(myData);
 
+        boolean isDetailed = false;
+
         if(matcherDetailed.find()){
 
-            myLine = Integer.parseInt(matcherDetailed.group(1));
+            myLine = matcherDetailed.group(1);
             myType = matcherDetailed.group(2);
 
             if(myType.equals("N")||myType.equals("F")||myType.equals("R")){
                 myDateTime = matcherDetailed.group(3);
             }
 
-            String theLineNumber = myLine.toString();
+            String theLineNumber = myLine;
             if(theLineNumber.length() == 1) theLineNumber = "0" + theLineNumber;
 
             // Add to log
             addCallToLog(theLineNumber,myType,"","","","",myDateTime,"","");
 
+            isDetailed = true;
+
+        }
+
+        if(myLine.length() == 1){
+            myLine = "0" + myLine;
+        }
+
+        // POST TO CLOUD
+        String url = rbUseSuppliedURL.isChecked() ? tbSuppliedURL.getText().toString() : lbGeneratedURL.getText().toString();
+
+        if (rbBasicUnit.isChecked())
+        {
+            if (myIndicator.equals("S") && !isDetailed)
+            {
+                postToCloud(url,myLine,myDateTime,myNumber,myName,myType,myIndicator,"",myDuration,myRings);
+            }
+        }
+        else
+        {
+            if (isDetailed)
+            {
+                if (!tbStatus.getText().toString().isEmpty())
+                {
+                    postToCloud(url,myLine,myDateTime,"","","","",myType,"","");
+                }
+            }
+            else
+            {
+                postToCloud(url,myLine,myDateTime,myNumber,myName,myType,myIndicator,"",myDuration,myRings);
+            }
         }
 
     }
